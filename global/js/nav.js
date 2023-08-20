0dc247f4ebf0161018d6d0bd9bf5fe876f0099f5
James <nex_e6@outlook.com>
2023-08-21 08:13:03 +1000

    Refactoring navbar

--- global/js/nav.js
+++ global/js/nav.js
@@ -1,38 +1,38 @@
 //This hides the navbar when scrolling
-function navBarAppear(ev) {
-	let nav = document.getElementById('navbar')
-	if (window.scrollY > 200) {
-		nav.style.position = 'sticky';
-		nav.style.top = '-80px';
-		window.setTimeout(function() {
-		nav.style.top = '0px';
-		}, 1);
-	} else {
-		nav.style.position = 'absolute';
-		nav.style.top = 'auto';
-	};
-}
+// function navBarAppear(ev) {
+// 	let nav = document.getElementById('navbar')
+// 	if (window.scrollY > 200) {
+// 		nav.style.position = 'sticky';
+// 		nav.style.top = '-80px';
+// 		window.setTimeout(function() {
+// 		nav.style.top = '0px';
+// 		}, 1);
+// 	} else {
+// 		nav.style.position = 'absolute';
+// 		nav.style.top = 'auto';
+// 	};
+// }
 
 //This does some (satasfying) maths to calculate what button to highlight
 //Due to the CSS of the right buttons, they are in the oppoite order to the left buttons
-function calcNavButtonPosition(docPos) {
-	return ((docPos - (document.querySelectorAll('.nav-item').length)) * -1) % document.querySelectorAll('.nav-item').length;
-}
+// function calcNavButtonPosition(docPos) {
+// 	return ((docPos - (document.querySelectorAll('.nav-item').length)) * -1) % document.querySelectorAll('.nav-item').length;
+// }
 
 //Highlight the nav button that links to the place that the user is scolled to
-function highlightNavButton() {
-	let scrollPosition = window.scrollY;
-	let navbarHeight = document.getElementById('navbar').offsetHeight;
-	document.querySelectorAll('.section').forEach(function(section, i) {
-		let topOffset = section.offsetTop;
-		if ((topOffset - scrollPosition - navbarHeight) < 0) {
-			if (document.querySelector('.nav-item.active')) {
-				document.querySelector('.nav-item.active').classList.remove('active');
-			}
-			document.querySelectorAll('.nav-item')[calcNavButtonPosition(i)].classList.add('active');
-		}
-	});
-}
+// function highlightNavButton() {
+// 	let scrollPosition = window.scrollY;
+// 	let navbarHeight = document.getElementById('navbar').offsetHeight;
+// 	document.querySelectorAll('.section').forEach(function(section, i) {
+// 		let topOffset = section.offsetTop;
+// 		if ((topOffset - scrollPosition - navbarHeight) < 0) {
+// 			if (document.querySelector('.nav-item.active')) {
+// 				document.querySelector('.nav-item.active').classList.remove('active');
+// 			}
+// 			document.querySelectorAll('.nav-item')[calcNavButtonPosition(i)].classList.add('active');
+// 		}
+// 	});
+// }
 
 
 function addNavClickEvents() {
